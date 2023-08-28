59133c9b4f143d036f1974a35fbb00adbe0f7e3c
ANDREE MATEO NARVAEZ ORTIZ <andreenarvaezmo@unimagdalena.edu.co>
2023-08-28 17:51:17 -0500

    clase 210

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Publisher } from '../../interfaces/hero.interfaces';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interfaces';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-new-hero-page',
@@ -8,8 +10,44 @@ import { Publisher } from '../../interfaces/hero.interfaces';
   ]
 })
 export class NewHeroPageComponent {
+
+  public heroForm = new FormGroup({
+    id               : new FormControl(''),
+    superhero        : new FormControl('', {nonNullable:true}),
+    publisher        : new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego        : new FormControl(''),
+    first_appearance : new FormControl(''),
+    characters       : new FormControl(''),
+    alt_img          : new FormControl(''),
+
+  });
+
   public publishers = [
     {id:'DC Comics', desc:'DC - Comics'},
     {id:'Marvel Comics', desc:'Marvel - Comics'},
   ];
+  constructor(private heroesService: HeroesService){
+
+  }
+  get currentHero():Hero{
+    const hero= this.heroForm.value as Hero;
+    return hero;
+  }
+  OnSubmit():void{
+    if(this.heroForm.invalid)return;
+    if(this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+        .subscribe(hero=>{
+          //TODO: mostrar snackbar
+        })
+        return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+      .subscribe(hero=>{
+        //TODO: mostrar snackbar y navigar a /heroes/edit/edit/hero.id
+      })
+
+  }
+
 }
